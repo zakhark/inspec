b1f1523c5a44a3c84f8003bbc8ef27afb01dff52
Christoph Hartmann <chris@lollyrock.com>
2017-11-21 15:51:07 +0100

    add test profile for skip-exception
    Signed-off-by: Christoph Hartmann <chris@lollyrock.com>

---
+++ test/unit/mock/profiles/skip-exception/controls/test2.rb
@@ -0,0 +1,8 @@
+# describe.one do
+#   describe xinetd_conf.services("chargen-dgram") do
+#     it { should be_disabled }
+#   end
+#   describe package("xinetd") do
+#     it { should_not be_installed }
+#   end
+# end
